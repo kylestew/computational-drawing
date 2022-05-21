7879e5d49d2d780350ac99a3f70708dc16f7df4d
Kyle Stewart <kylestew@gmail.com>
2022-05-21 10:46:22 -0600

    ...

--- sketches/thick-line.js
+++ sketches/thick-line.js
@@ -8,15 +8,19 @@ const settings = {
   dimensions: [1080, 1920],
 };
 
+const primary = "#e2725b";
+const secondary = "#30d5c8";
+const background = "#fffdd0";
+
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
+    context.fillStyle = background;
     context.fillRect(0, 0, width, height);
 
     const rowCount = 16;
     const rowHeight = height / (rowCount + 1.1);
     const vPadding = rowHeight;
-    const hPadding = width / 20;
+    const hPadding = width / 12;
 
     context.translate(hPadding, vPadding);
 
@@ -24,7 +28,7 @@ const sketch = () => {
       rectGrid(rowCount, 2, width - hPadding * 2, height - vPadding * 2)
     );
 
-    context.lineWidth = 1.2;
+    context.lineWidth = 1.6;
     context.lineCap = "round";
 
     for (let i = 0; i < lines.length; i++) {
@@ -34,13 +38,15 @@ const sketch = () => {
         line[1],
         [0, 0.25, 0.5, 0.75, 1],
         rowHeight,
-        i * 16 + 64,
+        i * 20 + 72,
         i % 2 == 0
       );
 
-      marks.forEach((line) => {
+      for (let j = 0; j < marks.length; j++) {
+        const line = marks[j];
+        context.strokeStyle = j % 2 == 0 ? primary : secondary;
         mark.line(context, line[0], line[1]);
-      });
+      }
     }
   };
 };
